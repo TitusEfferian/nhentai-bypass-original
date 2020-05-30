e221a54eace63a3842a9fb4d6fbe2f533f4c2e7e
titusefferian <titus.efferian@tokopedia.com>
2020-05-30 21:44:45 +0700

    dynamic source

--- index.js
+++ index.js
@@ -4,9 +4,8 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
-    const bucketId = req.query.bucketId;
-    const nhenPage = req.query.nhenPage;
-    request.get('https://i.nhentai.net/galleries/' + bucketId + '/' + nhenPage + '.jpg').pipe(res);
+    const source = req.query.source;
+    request.get(source).pipe(res);
 });
 
 app.listen(port, () => console.log(`listening at http://localhost:${port}`))
