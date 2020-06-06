e9515207beb63740b0908d328bcc518a753f5004
titusefferian <titus.efferian@tokopedia.com>
2020-06-06 09:16:31 +0700

    catch

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
-    const source = req.query.source;
-    request.get(source).pipe(res);
+    try {
+        const source = req.query.source;
+        request.get(source).pipe(res);
+    } catch (err) {
+        res.send(`<h3>error @catch ${JSON.stringify(err)}</h3>`)
+    }
 });
 
 app.listen(port, () => console.log(`listening at http://localhost:${port}`))
