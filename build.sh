3abc92a4d44f7f47096d3f1a78877cebff7e559f
titusefferian <titus.efferian@tokopedia.com>
2020-06-06 09:24:06 +0700

    build sh

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-git fetch && git rebase origin/master && gcloud builds submit --tag gcr.io/fleet-range-273715/nhentai-bypass-original
+git fetch && git rebase origin/master && gcloud builds submit --tag gcr.io/fleet-range-273715/nhentai-full-reader
